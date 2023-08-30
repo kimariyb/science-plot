66b777b6c49208a72c3a7fffbb6b50739780b30f
kimariyb <kimariyb@163.com>
2023-08-30 20:43:23 +0800

    update fraction and nics

--- line curve/NICS-1D/NICS-1D.py
+++ line curve/NICS-1D/NICS-1D.py
@@ -3,17 +3,39 @@ import proplot as pplt
 
 from proplot import rc
 
+# 用于初始化变量
+# 全局字体
+fontname = "Arial"
+# 全局字号，分别为普通字体、标签字体和标题字体
+fontsize = [10.5, 12, 14]
+# 颜色主题，可以修改自己喜欢的颜色或者 16 进制颜色
+colors = ['red', 'blue', 'green']
+# 判断 colors 是否为 None 或者空集合，如果为空集合或者 NONE，则使用默认的颜色集合
+if colors is None or colors == []:
+    # 默认为彩虹色
+    # 红 #FF0000
+    # 橙 #FF7F00
+    # 黄 #FFFF00
+    # 绿 #00FF00
+    # 青 #00FFFF
+    # 蓝 #0000FF
+    # 紫 #8B00FF
+    colors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#8B00FF']
+
 # 读取 Multiwfn 生成的 NICS-1D-all、sigma、pi 的数据
-nics_pi = pd.read_csv("NICS_1D_pi.txt", delimiter="\s+")
-nics_sigma = pd.read_csv("NICS_1D_sigma.txt", delimiter="\s+")
-nics_all = pd.read_csv("NICS_1D_all.txt", delimiter="\s+")
+# 如果需要修改，请修改 NICS_1D_pi.txt、NICS_1D_sigma.txt 以及 NICS_1D_all.txt
+nics_pi = pd.read_csv("NICS_1D_pi.txt", delim_whitespace=True, header=None)
+nics_sigma = pd.read_csv("NICS_1D_sigma.txt", delim_whitespace=True, header=None)
+nics_all = pd.read_csv("NICS_1D_all.txt", delim_whitespace=True, header=None)
 
 # 设置绘图的默认参数，如字体、字号等
-rc['font.family'] = "Arial"
+rc['font.family'] = fontname
+rc['title.size'] = fontsize[2]
+rc['label.size'] = fontsize[1]
+rc['font.size'] = fontsize[0]
+# 设置其他参数
 rc['tick.width'] = 1.3
 rc['meta.width'] = 1.3
-rc['font.size'] = 9.5
-rc['label.size'] = 12.5
 rc['label.weight'] = 'bold'
 rc['tick.labelweight'] = 'bold'
 rc['ytick.major.size'] = 4.6
@@ -22,9 +44,7 @@ rc['xtick.major.size'] = 4.6
 rc['xtick.minor.size'] = 2.5
 
 # 创建实例
-fig, ax = pplt.subplots(figsize=(5.4 * 0.9, 4 * 0.9), dpi=300)
-# 颜色 list，可以修改自己喜欢的颜色
-colors = ['off yellow', 'cherry red', 'true blue']
+fig, ax = pplt.subplots(figsize=(5.4 * 0.9, 4 * 0.9))
 
 # 绘制整体贡献的 NICS 曲线
 ax.plot(nics_all.iloc[:, -2], nics_all.iloc[:, -1], label='total', linewidth=1.2, color=colors[0])
@@ -39,13 +59,16 @@ ax_line = ax.axhline(y=0, color='black', linewidth=1.2)
 ax_line.set_zorder(0)
 
 # 设置图例
-ax.legend(loc='best', ncols=1, fontweight='bold', fontsize='12.5', frame=False, bbox_to_anchor=(0.95, 0.96))
+ax.legend(loc='ur', ncols=1, fontweight='bold', fontsize='12.5', frame=False, bbox_to_anchor=(0.95, 0.96))
 
 # 格式化图像
 fig.format(
     grid=False, ylabel='Shielding (in ppm)', xlabel='Position (in Å)',
     xlim=(-10, 10), xminorlocator=1, xlocator=2, ylim=(-30, 40), yminorlocator=5, ylocator=10
 )
+
+# 显示图像
+fig.show()
 
 # 保存图像
 fig.savefig("NICS.png", dpi=400, bbox_inches="tight")
--- line curve/fraction/distribution.py
+++ line curve/fraction/distribution.py
@@ -4,24 +4,15 @@ import toml
 
 from proplot import rc
 
-# 设置绘图的默认参数，如字体、字号等
-rc['font.name'] = "Arial"
-rc['title.size'] = 14
-rc['label.size'] = 12
-rc['font.size'] = 10.5
-rc['tick.width'] = 1.3
-rc['meta.width'] = 1.3
-rc['label.weight'] = 'bold'
-rc['tick.labelweight'] = 'bold'
-rc['ytick.major.size'] = 4.6
-rc['ytick.minor.size'] = 2.5
-rc['xtick.major.size'] = 4.6
-rc['xtick.minor.size'] = 2.5
-
-# 全局颜色，默认为 rainbow，可以自行修改成十六进制 list，例如 ['', '', '', '']
-COLORS = "rainbow"
-# 如果 COLORS = 'rainbow' 则将彩虹色赋值
-if COLORS == "rainbow":
+# 用于初始化变量
+# 全局字体
+fontname = "Arial"
+# 全局字号，分别为普通字体、标签字体和标题字体
+fontsize = [10.5, 12, 14]
+# 颜色主题，可以修改自己喜欢的颜色或者 16 进制颜色，一定要满足 colors list 中的元素要比 n 多
+colors = []
+# 判断 colors 是否为 None 或者空集合，如果为空集合或者 NONE，则使用默认的颜色集合
+if colors is None or colors == []:
     # 默认为彩虹色
     # 红 #FF0000
     # 橙 #FF7F00
@@ -30,7 +21,21 @@ if COLORS == "rainbow":
     # 青 #00FFFF
     # 蓝 #0000FF
     # 紫 #8B00FF
-    COLORS = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#8B00FF']
+    colors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#8B00FF']
+
+# 设置绘图的默认参数，如字体、字号等
+rc['font.name'] = fontname
+rc['title.size'] = fontsize[2]
+rc['label.size'] = fontsize[1]
+rc['font.size'] = fontsize[0]
+rc['tick.width'] = 1.3
+rc['meta.width'] = 1.3
+rc['label.weight'] = 'bold'
+rc['tick.labelweight'] = 'bold'
+rc['ytick.major.size'] = 4.6
+rc['ytick.minor.size'] = 2.5
+rc['xtick.major.size'] = 4.6
+rc['xtick.minor.size'] = 2.5
 
 
 def calculate_deltas(pH, n, K):
@@ -58,67 +63,50 @@ def calculate_deltas(pH, n, K):
     return distributions
 
 
-def plot_distribution_curve(n, K):
-    """
-    绘制分布分数-pH曲线图
-    :param n: n 元弱酸
-    :param K: 解离平衡常数 K
-    """
-    # 设置需要绘制的 pH 的范围为 0 ~ 14
-    pH_range = np.linspace(0, 14, 500)
-    # 初始化 distributions
-    distributions = np.zeros((n + 1, len(pH_range)))
-
-    # 计算每个 pH 值对应的分布分数
-    for i, pH in enumerate(pH_range):
-        distributions[:, i] = calculate_deltas(pH, n, K)
-
-    # 归一化分布分数
-    sum_distribution = np.sum(distributions, axis=0)
-    normalized_distributions = distributions / sum_distribution
-
-    # 创建实例
-    fig, ax = pplt.subplots(figsize=(5.4 * 0.9, 4 * 0.9), dpi=300)
-
-    # 如果想设置其他颜色循环可以使用 cycle 参数
-    # for i in range(n + 1):
-    #     ax.plot(pH_range, normalized_distributions[i, :], label=f'δ{n}({i + 1})', cycle='bmh')
-    # 或者直接用 pplt.rc.cycle = '538' 调整 cycle 参数
-    # 如果想使用自定义颜色可以使用 color 参数
-    # color_list = ['', '', '', '']
-    # for i in range(n + 1):
-    #     ax.plot(pH_range, normalized_distributions[i, :], label=f'δ{n}({i + 1})', color=color_list[i])
-
-    # 声明全局变量 COLORS，如果不需要这个颜色，可以直接 # 注释掉
-    global COLORS
-    # 绘制分布曲线
-    for i in range(n + 1):
-        ax.plot(pH_range, normalized_distributions[i, :], label=f'δ{n}({i + 1})', linewidth=1.3, color=COLORS[i])
-
-    # 设置图例
-    ax.legend(loc='ur', ncols=1, fontweight='bold', fontsize='12.5', frame=True)
-
-    # 格式化图像
-    fig.format(
-        grid=False, ylabel='Fraction', xlabel='pH Values',
-        xlim=(0, 14), xminorlocator=1, xlocator=2, ylim=(0, 1), yminorlocator=0.1, ylocator=0.2
-    )
-
-    # 显示图像
-    fig.show()
-
-    return fig, ax
-
-
 # 读取 equilibrium.toml 中的数据
 equilibrium = toml.load("equilibrium.toml")
 # 将每一个 K 的 value 记录到一个  list K_values 中
 K_values = [item['value'] for item in equilibrium.get("K")]
 
-# 根据 K_values list 拿到 n
-acid_num = len(K_values)
-# 绘制图像
-fig, ax = plot_distribution_curve(acid_num, K_values)
+# 根据 K_values list 拿到 n，n 代表多少元酸
+n = len(K_values)
+
+# 设置需要绘制的 pH 的范围为 0 ~ 14
+pH_range = np.linspace(0, 14, 500)
+# 初始化 distributions
+distributions = np.zeros((n + 1, len(pH_range)))
+# 计算每个 pH 值对应的分布分数
+for i, pH in enumerate(pH_range):
+    distributions[:, i] = calculate_deltas(pH, n, K_values)
+
+# 归一化分布分数
+sum_distribution = np.sum(distributions, axis=0)
+normalized_distributions = distributions / sum_distribution
+
+# 创建实例
+fig, ax = pplt.subplots(figsize=(5.4 * 0.9, 4 * 0.9))
+
+
+# 如果想设置其他颜色循环可以使用 cycle 参数
+# for i in range(n + 1):
+#     ax.plot(pH_range, normalized_distributions[i, :], label=f'δ{n}({i + 1})', cycle='bmh')
+# 或者直接用 pplt.rc.cycle = '538' 调整 cycle 参数
+
+# 绘制分布曲线
+for i in range(n + 1):
+    ax.plot(pH_range, normalized_distributions[i, :], label=f'δ{n}({i + 1})', linewidth=1.3, color=colors[i])
+
+# 设置图例
+ax.legend(loc='ur', ncols=1, fontweight='bold', fontsize='12.5', frame=True)
+
+# 格式化图像
+fig.format(
+    grid=False, ylabel='Fraction', xlabel='pH Values',
+    xlim=(0, 14), xminorlocator=1, xlocator=2, ylim=(0, 1), yminorlocator=0.1, ylocator=0.2
+)
+
+# 显示图像
+fig.show()
 
 # 保存图像
 fig.savefig("delta.png", dpi=400, bbox_inches="tight")
